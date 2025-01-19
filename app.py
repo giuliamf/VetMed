7c64c877ed7b3a735ae8616fe6c76f6e013d051b
Célio Eduardo <59587358+celio-eduardo@users.noreply.github.com>
2025-01-19 17:00:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/veterinario_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/veterinaria_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
