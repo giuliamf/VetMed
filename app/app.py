29215d38a4babcb7b7c80ee08eb58200b5cb91c1
giulia <giuliamferreira01@gmail.com>
2025-01-31 14:03:05 -0300

    sei la adicionei um monte de coisa aqui

--- app/app.py
+++ app/app.py
@@ -4,30 +4,37 @@ app = Flask(__name__)
 
 # Simulação de dados vindos do banco de dados
 pacientes = [
-    {"nome": "Marley (Ana Luiza Campos)"},
-    {"nome": "Catherine (Célio Eduardo Júnior)"},
-    {"nome": "Thor (João Pedro Souza)"},
-    {"nome": "Mel (Fernanda Oliveira)"},
-    {"nome": "Rex (Carlos Eduardo Santos)"}
+    {'nome': 'Marley (Ana Luiza Campos)'},
+    {'nome': 'Catherine (Célio Eduardo Júnior)'},
+    {'nome': 'Thor (João Pedro Souza)'},
+    {'nome': 'Mel (Fernanda Oliveira)'},
+    {'nome': 'Rex (Carlos Eduardo Santos)'}
 ]
 
 
-@app.route("/")
+@app.route('/')
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
 
-@app.route("/inicio")
+@app.route('/inicio')
 def inicio():
-    return render_template("inicio.html")
+    return render_template('inicio.html')
 
 
-@app.route("/cadastros")
+# Tela de agenda e suas funcionalidades
+@app.route('/agenda')
+def agenda():
+    return render_template('agenda.html')
+
+
+# Tela de cadastros e suas funcionalidades
+@app.route('/cadastros')
 def cadastro():
-    return render_template("cadastros.html")
+    return render_template('cadastros.html')
 
 
-@app.route("/pacientes")
+@app.route('/pacientes')
 def pacientes_page():
     return render_template('pacientes.html', pacientes=pacientes)
 
@@ -37,5 +44,19 @@ def get_pacientes():
     return jsonify(pacientes)
 
 
-if __name__ == "__main__":
+# Tela de consultas e suas funcionalidades
+@app.route('/consultas')
+def consultas():
+    return render_template('consultas.html')
+
+
+# Tela do financeiro e suas funcionalidades
+@app.route('/financeiro')
+def financeiro():
+    return render_template('financeiro.html')
+
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
