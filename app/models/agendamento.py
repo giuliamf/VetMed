1eefecb164bc55302c300dbb27597f89ab8e8702
celio-eduardo <celio.pic@gmail.com>
2025-02-01 00:59:17 -0300

    Atualizações

--- app/models/agendamento.py
+++ app/models/agendamento.py
@@ -1,19 +1,124 @@
-from app import db
-
-class Agendamento(db.Model):
-    __tablename__ = 'agendamentos'
-
-    id_agendamento = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_tutor = db.Column(db.Integer, db.ForeignKey('tutores.id_tutor'), nullable=False)
-    id_animal = db.Column(db.Integer, db.ForeignKey('animais.id_animal'), nullable=False)
-    data = db.Column(db.Date, nullable=False)
-    hora = db.Column(db.Time, nullable=False)
-    id_status = db.Column(db.Integer, db.ForeignKey('status.id_status'), nullable=False)
-
-    # Relacionamentos
-    tutor = db.relationship('Tutor', backref='agendamentos')
-    animal = db.relationship('Animal', backref='agendamentos')
-    status = db.relationship('Status', backref='agendamentos')
+class Agendamento:
+    def __init__(self, id_agendamento, id_tutor, id_animal, data, hora, id_status):
+        self.id_agendamento = id_agendamento
+        self.id_tutor = id_tutor
+        self.id_animal = id_animal
+        self.data = data
+        self.hora = hora
+        self.id_status = id_status
 
     def __repr__(self):
         return f"<Agendamento {self.id_agendamento}>"
+    
+    def criar_agendamento(data):
+        """
+        Cria um novo agendamento no banco de dados.
+        Recebe um dicionário `data` com os campos do agendamento.
+        """
+        # Validar os dados
+        valido, mensagem = AgendamentoValidator.validar_criar_agendamento(data)
+        if not valido:
+            raise ValueError(mensagem)
+
+        # Extrair os valores do dicionário
+        id_tutor = data['id_tutor']
+        id_animal = data['id_animal']
+        data_agendamento = datetime.strptime(data['data'], '%d/%m/%Y').date()
+        hora_agendamento = datetime.strptime(data['hora'], '%H:%M').time()
+        id_status = data['id_status']
+
+        # Inserir no banco de dados
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = sql.SQL("""
+            INSERT INTO agendamentos (id_tutor, id_animal, data, hora, id_status)
+            VALUES (%s, %s, %s, %s, %s)
+            RETURNING id_agendamento
+        """)
+        cursor.execute(query, (id_tutor, id_animal, data_agendamento, hora_agendamento, id_status))
+        id_agendamento = cursor.fetchone()[0]
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return id_agendamento
+
+    def buscar_agendamento_por_id(id_agendamento):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = sql.SQL("""
+            SELECT * FROM agendamentos WHERE id_agendamento = %s
+        """)
+        cursor.execute(query, (id_agendamento,))
+        agendamento = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if agendamento:
+            return Agendamento(*agendamento)
+        return None
+
+    def atualizar_agendamento(id_agendamento, id_tutor, id_animal, data, hora, id_status):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = sql.SQL("""
+            UPDATE agendamentos
+            SET id_tutor = %s, id_animal = %s, data = %s, hora = %s, id_status = %s
+            WHERE id_agendamento = %s
+        """)
+        cursor.execute(query, (id_tutor, id_animal, data, hora, id_status, id_agendamento))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        
+    CAMPOS_VALIDOS = {'id_tutor', 'id_animal', 'data', 'hora', 'id_status'}
+    
+    def atualizar_agendamento(id_agendamento, **kwargs):
+        """
+        Atualiza os campos fornecidos em kwargs para o agendamento com o id_agendamento especificado.
+        """
+        # Validar os dados
+        valido, mensagem = AgendamentoValidator.validar_atualizar_agendamento(kwargs)
+        if not valido:
+            raise ValueError(mensagem)
+
+        # Converter 'data' e 'hora' para os tipos corretos, se presentes
+        if 'data' in kwargs:
+            kwargs['data'] = datetime.strptime(kwargs['data'], '%d/%m/%Y').date()
+        if 'hora' in kwargs:
+            kwargs['hora'] = datetime.strptime(kwargs['hora'], '%H:%M').time()
+
+        # Construir a query dinamicamente
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        campos = []
+        valores = []
+        for campo, valor in kwargs.items():
+            campos.append(sql.Identifier(campo))
+            valores.append(valor)
+        valores.append(id_agendamento)  # Adicionar o id_agendamento no final
+
+        query = sql.SQL("""
+            UPDATE agendamentos
+            SET {campos}
+            WHERE id_agendamento = %s
+        """).format(
+            campos=sql.SQL(', ').join(
+                sql.SQL("{} = %s").format(campo) for campo in campos
+            )
+        )
+
+        cursor.execute(query, valores)
+        conn.commit()
+        cursor.close()
+        conn.close()
+    
+    
+    def deletar_agendamento(id_agendamento):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = sql.SQL("""
+            DELETE FROM agendamentos WHERE id_agendamento = %s
+        """)
+        cursor.execute(query, (id_agendamento,))
+        conn.commit()
+        cursor.close()
+        conn.close()
--- app/routes/agendamentos.py
+++ app/routes/agendamentos.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Flask, request, jsonify
 from app.models.agendamento import Agendamento
 from app.validators.agendamento_validator import AgendamentoValidator
 from app import db
@@ -6,27 +6,101 @@ from datetime import datetime
 
 agendamentos_bp = Blueprint('agendamentos', __name__)
 
-@agendamentos_bp.route('/agendamentos', methods=['POST'])
-def criar_agendamento():
-    data = request.get_json()
+app = Flask(__name__)
 
-    # Validar dados
-    valido, mensagem = AgendamentoValidator.validar_criar_agendamento(data)
-    if not valido:
-        return jsonify({"erro": mensagem}), 400
+from flask import request, jsonify
+from datetime import datetime
 
-    # Converter data e hora para os formatos corretos
-    data_agendamento = datetime.strptime(data['data'], '%d/%m/%Y').date()
-    hora_agendamento = datetime.strptime(data['hora'], '%H:%M').time()
+@app.route('/agendamentos', methods=['POST'])
+def criar_agendamento_route():
+    try:
+        # Obter os dados do corpo da requisição
+        dados = request.json
 
-    # Criar agendamento
-    novo_agendamento = Agendamento(
-        id_tutor=data['id_tutor'],
-        id_animal=data['id_animal'],
-        data=data_agendamento,
-        hora=hora_agendamento,
-        id_status=data['id_status']
-    )
-    db.session.add(novo_agendamento)
-    db.session.commit()
-    return jsonify({"mensagem": "Agendamento criado com sucesso!", "id_agendamento": novo_agendamento.id_agendamento}), 201
+        # Validar os dados antes de criar o agendamento
+        valido, mensagem = AgendamentoValidator.validar_criar_agendamento(dados)
+        if not valido:
+            return jsonify({"erro": mensagem}), 400  # Bad Request
+
+        # Converter a data e hora para os tipos corretos
+        data_agendamento = datetime.strptime(dados['data'], '%d/%m/%Y').date()
+        hora_agendamento = datetime.strptime(dados['hora'], '%H:%M').time()
+
+        # Criar o agendamento no banco de dados
+        id_agendamento = criar_agendamento(
+            dados['id_tutor'],
+            dados['id_animal'],
+            data_agendamento,
+            hora_agendamento,
+            dados['id_status']
+        )
+
+        # Retornar a resposta com o ID do agendamento criado
+        return jsonify({"id_agendamento": id_agendamento}), 201  # Created
+
+    except ValueError as e:
+        # Capturar erros de validação ou conversão de dados
+        return jsonify({"erro": str(e)}), 400  # Bad Request
+
+    except Exception as e:
+        # Capturar outros erros inesperados (ex: erros no banco de dados)
+        return jsonify({"erro": f"Erro interno no servidor: {str(e)}"}), 500  # Internal Server Error
+
+@app.route('/agendamentos/<int:id_agendamento>', methods=['GET'])
+def buscar_agendamento_route(id_agendamento):
+    agendamento = buscar_agendamento_por_id(id_agendamento)
+    if agendamento:
+        return jsonify({
+            "id_agendamento": agendamento.id_agendamento,
+            "id_tutor": agendamento.id_tutor,
+            "id_animal": agendamento.id_animal,
+            "data": agendamento.data,
+            "hora": agendamento.hora,
+            "id_status": agendamento.id_status
+        })
+    return jsonify({"mensagem": "Agendamento não encontrado"}), 404
+
+from flask import request, jsonify
+from datetime import datetime
+
+from flask import request, jsonify
+from datetime import datetime
+
+@app.route('/agendamentos/<int:id_agendamento>', methods=['PUT'])
+def atualizar_agendamento_route(id_agendamento):
+    try:
+        # Obter os dados do corpo da requisição
+        dados = request.json
+
+        # Validar os dados antes de atualizar o agendamento
+        valido, mensagem = AgendamentoValidator.validar_atualizar_agendamento(dados)
+        if not valido:
+            return jsonify({"erro": mensagem}), 400  # Bad Request
+
+        # Converter a data e hora para os tipos corretos, se presentes
+        if 'data' in dados:
+            dados['data'] = datetime.strptime(dados['data'], '%d/%m/%Y').date()
+        if 'hora' in dados:
+            dados['hora'] = datetime.strptime(dados['hora'], '%H:%M').time()
+
+        # Atualizar o agendamento no banco de dados
+        atualizar_agendamento(id_agendamento, **dados)
+
+        # Retornar a resposta de sucesso
+        return jsonify({"mensagem": "Agendamento atualizado com sucesso"}), 200  # OK
+
+    except ValueError as e:
+        # Capturar erros de validação ou conversão de dados
+        return jsonify({"erro": str(e)}), 400  # Bad Request
+
+    except Exception as e:
+        # Capturar outros erros inesperados (ex: erros no banco de dados)
+        return jsonify({"erro": f"Erro interno no servidor: {str(e)}"}), 500  # Internal Server Error
+
+@app.route('/agendamentos/<int:id_agendamento>', methods=['DELETE'])
+def deletar_agendamento_route(id_agendamento):
+    deletar_agendamento(id_agendamento)
+    return jsonify({"mensagem": "Agendamento deletado com sucesso"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
