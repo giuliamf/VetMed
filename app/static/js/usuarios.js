eef5cd7e65bf139d4a1842ae54fbd66a145bb737
❥ giulia <giuliamferreira01@gmail.com>
2025-02-09 20:54:39 -0300

    updt usuarios

--- app/static/js/usuarios.js
+++ app/static/js/usuarios.js
@@ -24,6 +24,40 @@ function toggleEspecialidade() {
     }
 }
 
+function editarUsuario(id) {
+    fetch("/api/usuarios")
+        .then(response => response.json())
+        .then(usuarios => {
+            let usuario = usuarios.find(u => u.id === parseInt(id));
+            if (!usuario) {
+                console.error("Usuário não encontrado!")
+                return
+            }
+
+            fetch("/cadastro_usuario")
+                .then(response => response.text())
+                .then(html => {
+                    document.getElementById("popupContainer").innerHTML = html;
+                    document.getElementById("cadastro-popup").style.display = "flex";
+
+                    document.getElementById("nome").value = usuario.nome;
+                    document.getElementById("email").value = usuario.email;
+                    document.getElementById("senha").value = usuario.senha;
+                    document.getElementById("cargo").value = usuario.cargo;
+
+                    toggleEspecialidade()
+
+                    if (usuario.cargo === "vet") {
+                        document.getElementById("especialidade").value = usuario.especialidade;
+                    } else {
+                        document.getElementById("especialidade").value = "";
+                    }
+                    document.getElementById("cargo").addEventListener("change", toggleEspecialidade);
+                })
+            .catch(error => console.error("Erro ao carregar popup: ", error));
+        })
+     .catch(error => console.error("Erro ao buscar usuários:", error));
+}
 
 function fecharPopupCadastro() {
     document.getElementById("cadastro-popup").style.display = "none";
@@ -32,3 +66,4 @@ function fecharPopupCadastro() {
 
 window.fecharPopupCadastro = fecharPopupCadastro;
 window.toggleEspecialidade = toggleEspecialidade;
+window.editarUsuario = editarUsuario;
