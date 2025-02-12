14f575eab193a6c206d94ea2421b7b90c3419b15
❥ giulia <giuliamferreira01@gmail.com>
2025-02-12 04:09:58 -0300

    usuario

--- app/routes/usuario.py
+++ app/routes/usuario.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify, render_template
 from app.database import execute_sql
 
+from app.utils.criptografia_senhas import criptografar_senha
+
 usuarios_bp = Blueprint('usuarios', __name__)
 
 
@@ -8,7 +10,7 @@ usuarios_bp = Blueprint('usuarios', __name__)
 def get_usuarios():
     """ Retorna a lista de usuários do banco de dados """
     try:
-        query = "SELECT * FROM Usuario ORDER BY id_usuario ASC"
+        query = "SELECT * FROM Usuario WHERE cargo != 'adm' ORDER BY id_usuario ASC"
         usuarios = execute_sql(query, fetch_all=True)
 
         usuarios_lista = [
@@ -22,15 +24,155 @@ def get_usuarios():
             for u in usuarios
         ]
 
-        for usuario in usuarios_lista:
-            if usuario['cargo'] == 'vet':
-                query = "SELECT * FROM Veterinario WHERE email = %s"
-                vet = execute_sql(query, (usuario['email'],), fetch_one=True)
-                usuario['especialidade'] = vet[1]
-
         return jsonify(usuarios_lista), 200
     except Exception as e:
         return jsonify({"erro": f"Erro ao buscar usuários: {str(e)}"}), 500
+
+
+@usuarios_bp.route('/cadastro_usuario', methods=['POST'])
+def cadastro_usuario():
+    """ Cadastro de novo usuário """
+    if not request.json:
+        return jsonify({"erro": "Nenhum dado JSON foi recebido"}), 400
+
+    data = request.json
+
+    if not all(key in data for key in ["nome", "email", "senha", "cargo"]):
+        return jsonify({"erro": "Campos obrigatórios ausentes!"}), 400
+
+    if not all(data.values()):
+        return jsonify({"erro": "Preencha todos os campos!"}), 400
+
+    # criptografar a senha recebida
+    data['senha'] = criptografar_senha(data['senha'])
+
+    try:
+        query = """
+            INSERT INTO Usuario (email, nome, senha, cargo)
+            VALUES (%s, %s, %s, %s)
+        """
+        params = (
+            data['email'],
+            data['nome'],
+            data['senha'],
+            data['cargo']
+        )
+        execute_sql(query, params)
+
+        # Se o cargo for 'vet', cadastrar na tabela Veterinario
+        if data['cargo'] == 'vet':
+            # Obter o id a partir do execute_sql anterior
+            id_usuario = execute_sql(
+                "SELECT id_usuario FROM Usuario WHERE email = %s", (data['email'],), fetch_one=True)
+
+            query_vet = """
+                INSERT INTO Veterinario (id_veterinario, id_especialidade)
+                VALUES (%s, %s)
+            """
+            params_vet = (
+                id_usuario[0],
+                data['especialidade']
+            )
+            execute_sql(query_vet, params_vet)
+
+        return jsonify({"mensagem": "Usuário cadastrado com sucesso!"}), 201
+    except Exception as e:
+        return jsonify({"erro": f"Erro ao cadastrar usuário: {str(e)}"}), 500
+
+
+@usuarios_bp.route('/api/usuarios/<int:usuario_id>', methods=['GET', 'PUT'])
+def editar_usuario(usuario_id):
+    """ Rota para editar um usuário """
+    if request.method == 'GET':
+        # Buscar usuário pelo ID no banco de dados
+        query = "SELECT * FROM Usuario WHERE id_usuario = %s"
+        resultado = execute_sql(query, (usuario_id,), fetch_one=True)
+
+        if not resultado:
+            return jsonify({"erro": "Usuário não encontrado!"}), 404
+
+        usuario = {
+            "id": resultado[0],
+            "email": resultado[1],
+            "nome": resultado[2],
+            "senha": resultado[3],
+            "cargo": resultado[4]
+        }
+
+        return jsonify(usuario), 200
+
+    if request.method == 'PUT':
+        data = request.json
+
+        if not all(key in data for key in ["nome", "email", "senha", "cargo"]):
+            return jsonify({"erro": "Campos obrigatórios ausentes!"}), 400
+
+        if not all(data.values()):
+            return jsonify({"erro": "Preencha todos os campos!"}), 400
+
+        # Conferir se a senha foi modificada
+        query_senha = "SELECT senha FROM Usuario WHERE id_usuario = %s"
+        senha_atual = execute_sql(query_senha, (usuario_id,), fetch_one=True)
+
+        if senha_atual[0] != data['senha']:
+            # criptografar a nova senha recebida
+            data['senha'] = criptografar_senha(data['senha'])
+
+        try:
+            query = """
+                UPDATE Usuario
+                SET email = %s, nome = %s, senha = %s, cargo = %s
+                WHERE id_usuario = %s
+            """
+            params = (
+                data['email'],
+                data['nome'],
+                data['senha'],
+                data['cargo'],
+                usuario_id
+            )
+            execute_sql(query, params)
+
+            # Se o cargo for 'vet', atualizar na tabela Veterinario
+            if data['cargo'] == 'vet':
+                try:
+                    query_vet = """
+                        UPDATE Veterinario
+                        SET id_especialidade = %s
+                        WHERE id_veterinario = %s
+                    """
+                    params_vet = (
+                        data['especialidade'],
+                        usuario_id
+                    )
+                    execute_sql(query_vet, params_vet)
+
+                except Exception as e:
+                    return jsonify({"erro": f"Erro ao atualizar especialidade: {str(e)}"}), 500
+
+            return jsonify({"mensagem": "Usuário atualizado com sucesso!"}), 200
+        except Exception as e:
+            return jsonify({"erro": f"Erro ao atualizar usuário: {str(e)}"}), 500
+
+
+@usuarios_bp.route('/api/especialidades', methods=['GET'])
+def get_especialidades():
+    """ Retorna a lista de especialidades """
+    try:
+        query = "SELECT * FROM Especialidade ORDER BY nome ASC"
+        especialidades = execute_sql(query, fetch_all=True)
+
+        especialidades_lista = [
+            {
+                "id": e[0],
+                "nome": e[1]
+            }
+            for e in especialidades
+        ]
+
+        return jsonify(especialidades_lista), 200
+    except Exception as e:
+        return jsonify({"erro": f"Erro ao buscar especialidades: {str(e)}"}), 500
 
 
 # Rotas para renderizar páginas HTML
--- app/static/js/usuario.js
+++ app/static/js/usuario.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", carregarUsuarios);
+
 document.getElementById("novoCadastro").addEventListener("click", function () {
     fetch("/cadastro_usuario")
         .then(response => response.text())
@@ -13,15 +15,109 @@ document.getElementById("novoCadastro").addEventListener("click", function () {
         .catch(error => console.error("Erro ao carregar popup: ", error));
 });
 
-function toggleEspecialidade() {
-    var cargo = document.getElementById("cargo").value;
-    var especialidadeContainer = document.getElementById("especialidade-container");
+function carregarUsuarios() {
+    fetch("/api/usuarios")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erro ao buscar usuários");
+            }
+            return response.json();
+        })
+        .then(usuarios => {
+            let tabela = document.getElementById("tabelaUsuarios");
+            tabela.innerHTML = ""; // Limpa a tabela antes de recarregar os dados
 
-    if (cargo === "vet") {
-        especialidadeContainer.style.display = "block";
-    } else {
-        especialidadeContainer.style.display = "none";
+            usuarios.forEach(usuario => {
+                let linha = document.createElement("tr");
+                linha.innerHTML = `
+                    <td>${usuario.nome} - ${usuario.cargo === 'vet' ? 'Veterinário(a)' : usuario.cargo === 'sec' ? 'Secretário(a)' : usuario.cargo}</td>
+                    <td><button onclick="editarUsuario('${usuario.id}')">Editar</button></td>
+                `;
+                tabela.appendChild(linha);
+            });
+        })
+        .catch(error => console.error("Erro ao carregar usuários:", error));
+}
+
+function enviarFormulario() {
+    const form = document.getElementById("cadastroForm");
+    const formData = new FormData(form);
+
+    // Só considera a especialidade, caso ela não seja undefined
+    const cargo = formData.get("cargo") || "";
+    const especialidade = cargo === "vet" ? formData.get("especialidade")?.trim() || "" : undefined;
+
+    const dados = {
+        nome: formData.get("nome")?.trim() || "",
+        email: formData.get("email")?.trim() || "",
+        senha: formData.get("senha")?.trim() || "",
+        cargo: cargo,
+        ...(especialidade !== undefined && { especialidade }) // Adiciona a especialidade apenas caso ela exista (vet)
+    };
+
+    // Validação: Todos os campos devem estar preenchidos
+    if (!dados.nome || !dados.email || !dados.senha || !dados.cargo) {
+        alert("Por favor, preencha todos os campos.");
+        return;
     }
+
+    if (dados.cargo === "vet" && !dados.especialidade) {
+        alert("Por favor, preencha a especialidade.");
+        return;
+    }
+
+    fetch("/cadastro_usuario", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dados),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erro ao cadastrar usuário");
+            }
+            return response.json();
+        })
+        .then(() => {
+            alert(response.mensagem || "Tutor cadastrado com sucesso!");
+            fecharPopupCadastro();
+            carregarUsuarios();
+        })
+        .catch(error => {
+        console.error("Erro ao cadastrar tutor:", error);
+        alert(error.message)
+    });
+}
+
+function salvarEdicaoUsuario(id) {
+    const usuarioAtualizado = {
+        nome: document.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        senha: document.getElementById("senha").value,
+        cargo: document.getElementById("cargo").value,
+        especialidade: document.getElementById("especialidade").value, // caso não seja vet, o valor é vazio
+    };
+
+    fetch(`/api/usuarios/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(usuarioAtualizado)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erro ao atualizar usuário");
+            }
+            return response.json();
+        })
+        .then(() => {
+            alert(data.mensagem);
+            fecharPopupCadastro();
+            carregarUsuarios();
+        })
+        .catch(error => console.error("Erro ao atualizar usuário:", error));
 }
 
 function editarUsuario(id) {
@@ -53,10 +149,49 @@ function editarUsuario(id) {
                         document.getElementById("especialidade").value = "";
                     }
                     document.getElementById("cargo").addEventListener("change", toggleEspecialidade);
+                    document.getElementById("cadastroForm").dataset.usuarioId = id;
+                    document.getElementById("cadastro-popup").style.display = "block";
                 })
-            .catch(error => console.error("Erro ao carregar popup: ", error));
+                .catch(error => console.error("Erro ao carregar popup: ", error));
         })
-     .catch(error => console.error("Erro ao buscar usuários:", error));
+        .catch(error => console.error("Erro ao buscar usuários:", error));
+}
+
+document.getElementById("cadastroForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const id = event.target.dataset.usuarioId;
+    salvarEdicaoUsuario(id);
+});
+
+function toggleEspecialidade() {
+    let cargo = document.getElementById("cargo").value;
+    let especialidadeContainer = document.getElementById("especialidade-container");
+    let especialidadeSelect = document.getElementById("especialidade");
+
+    if (cargo === "vet") {
+        especialidadeContainer.style.display = "block";
+        carregarEspecialidades();
+    } else {
+        especialidadeContainer.style.display = "none";
+        especialidadeSelect.innerHTML = '<option value="">Selecione</option>'
+    }
+}
+
+function carregarEspecialidades() {
+    fetch("/api/especialidades")
+        .then(response => response.json())
+        .then(especialidades => {
+            let especialidadeSelect = document.getElementById("especialidade");
+            especialidadeSelect.innerHTML = '<option value="">Selecione</option>';
+
+            especialidades.forEach(especialidade => {
+                let option = document.createElement("option");
+                option.value = especialidade.id;
+                option.innerText = especialidade.nome;
+                especialidadeSelect.appendChild(option);
+            });
+        })
+        .catch(error => console.error("Erro ao carregar especialidades:", error));
 }
 
 function fecharPopupCadastro() {
